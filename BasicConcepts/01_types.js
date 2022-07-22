21a5ba569a2e4a7637fd12ddc461b2b033f553f0
Ariuna8 <98133228+Ariuna8@users.noreply.github.com>
2022-07-21 17:50:49 -0700

    update basic concepts (#1)

--- BasicConcepts/01_types.js
+++ BasicConcepts/01_types.js
@@ -1,21 +1,41 @@
-console.log(1+'2')          // '12'
+//null,undefined, boolean, number, string, object(non-primit), symbol
+console.log(typeof 0)  // number
+console.log(typeof true) // boolean
+console.log(typeof 'JS')  // string
+console.log(typeof undefined) // undefined
+console.log(typeof Math) //object
+console.log(typeof Symbol('JS')) // symbol
+console.log(typeof null) // typeof null actually, not object
+console.log(typeof NaN) // number
+//============================
+console.log(Boolean('')) // false
+console.log(Boolean('Hello')) // true
+console.log(Boolean(' ')) // true
+console.log(Boolean('0')) // true
+console.log(Boolean(0)) // false
+console.log(Boolean(null)) // false
+console.log(Boolean([])) // true
+console.log(Boolean({})) // true
+console.log(Boolean(function (){})) // true
+//==============================
+console.log(1 + '2')        // '12'
 console.log('' + 1 + 0)     // '10'
-console.log('' - 1 + 0)     // -1
+console.log('' - 1 + 0)     // -1   '-'is not used for concat string => '' => as number 0
 console.log('3' * '8')      // 24
+console.log(4 + 10 + 'px')  // '14px'
 console.log('px' + 5 + 10)  // 'px510'
 console.log('42' - 40)      //  2
 console.log('42px' - 2)     // NaN
-console.log(null + 2)       // 2
+console.log(null + 2)       // 2   null to number => 0
 console.log(undefined + 42) // NaN
-
 // ========== VS ==============
 console.log(2 == '2')  //true
 console.log(2 === '2') //false
-console.log(undefined == false) //false
+console.log(undefined == null) //true
+console.log(undefined === null) //false
 console.log('0' == false) // true
 console.log('0' == 0) // true
 console.log(0 == 0) // true
-
 //==============================
 console.log(false == '') //true
 console.log(false == []) //true
@@ -26,3 +46,7 @@ console.log('' == {}) //false
 console.log(0 == []) //true
 console.log(0 == {}) //false
 console.log(0 == null) //false
+
+// Difference between null and undefined?
+// null - no any value in variable
+// undefined - used when variable wasn't declared or variable declared without value
--- BasicConcepts/02_values.js
+++ BasicConcepts/02_values.js
@@ -1,16 +1,16 @@
-// let a = 42
-// let b = a
-// b++
-// console.log('a', a) // 42
-// console.log('b', b) // 43
+let a = 42
+let b = a
+b++
+console.log('a', a) // 42
+console.log('b', b) // 43
 
 let a = [1,2,3]
-let b = a
+let b = a //(a.concat() - return copy of array)
 b.push(4)
 
 let c = [1,2,3,4]
 console.log('a', a) // a [ 1, 2, 3, 4 ]
-console.log('b', b) // b [ 1, 2, 3, 4 ]
+console.log('b', b) // b [ 1, 2, 3, 4 ] // reference on a
 
 console.log(a === b) //true
 console.log(a === c)  //false
