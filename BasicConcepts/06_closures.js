2e41885b5c3b2379949f4223a98d12c65caa23d7
Ariuna <ariun0188@gmail.com>
2022-07-22 13:20:44 -0700

    added promise, async, await

--- BasicConcepts/06_closures.js
+++ BasicConcepts/06_closures.js
@@ -1,45 +1,45 @@
-// //замыкание - момент когда функция имеет доступ к переменным из вышестоящего скопа
-// function sayHelloTo(name){
-//   const message = 'Hello ' + name
-//
-//   return function(){
-//     console.log(message)
-//   }
-// }
-//
-// const helloToElena = sayHelloTo('Elena')
-// const helloToIgor = sayHelloTo('Igor')
-// console.log(helloToElena)
-// console.log(helloToIgor)
-// helloToElena() // Hello Elena
-// helloToIgor() // Hello Igor
-//
-// function createFrameworkManager(){
-//   const fw = ['an', 're']
-//   return {
-//     print: function (){
-//       console.log(fw)
-//     },
-//     add: function (framework){
-//       fw.push(framework)
-//     }
-//   }
-// }
-// const manager = createFrameworkManager()
-// console.log(manager)
-// manager.print()
-// manager.add('view')
-// manager.print()
-//
-//
+//замыкание - момент когда функция имеет доступ к переменным из вышестоящего скопа
+function sayHelloTo(name){
+  const message = 'Hello ' + name
+
+  return function(){
+    console.log(message)
+  }
+}
+
+const helloToElena = sayHelloTo('Elena')
+const helloToIgor = sayHelloTo('Igor')
+console.log(helloToElena)
+console.log(helloToIgor)
+helloToElena() // Hello Elena
+helloToIgor() // Hello Igor
+
+function createFrameworkManager(){
+  const fw = ['an', 're']
+  return {
+    print: function (){
+      console.log(fw)
+    },
+    add: function (framework){
+      fw.push(framework)
+    }
+  }
+}
+const manager = createFrameworkManager()
+console.log(manager)
+manager.print()
+manager.add('view')
+manager.print()
+
+
 //setTimeOut
-// const fib = [1,2,3,5,8,13]
-// for(var i = 0; i < fib.length; i++){
-//   setTimeout(function (){
-//     console.log(`fib[${i}] = ${fib[i]}`)
-//   }, 1500)
-// } // undefined
-//
+const fib = [1,2,3,5,8,13]
+for(var i = 0; i < fib.length; i++){
+  setTimeout(function (){
+    console.log(`fib[${i}] = ${fib[i]}`)
+  }, 1500)
+} // undefined
+
 const fib = [1,2,3,5,8,13]
 for(var i = 0; i < fib.length; i++){
   (function(j){
