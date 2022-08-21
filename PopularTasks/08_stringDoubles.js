6e31416ef7b947c851154bfc37d9899cfc5379c3
Ariuna <ariun0188@gmail.com>
2022-08-20 18:07:14 -0700

    added task array

--- PopularTasks/08_stringDoubles.js
+++ PopularTasks/08_stringDoubles.js
@@ -1,19 +1,21 @@
 //In this Kata, you will write a function doubles
 // that will remove double string characters that are adjacent to each other.
-
-let s = 'abbcccdddda';
-function doubles(s){
-  let arr =['a',];
-  for(let i=0;i<s.length;i++){
-    if(arr[arr.length-1]!==s[i]) // sets it to the last element of arr
-    {
-      arr.push(s[i]);
-      console.log(arr, `${i}`)
-     } else {
-      arr.pop()
-    console.log(arr, `${i}`)
-    }
-  }
-  return arr.join('')
+let string = 'abbcccdddda'
+function removeDuplicate(x){
+  let a = x.split('');
+  let x2 = [];
+  for (let i in a)
+    if(x2.indexOf(a[i]) === -1) x2.push(a[i])
+  return  x2.join('');
 }
-console.log(doubles(s))
+
+console.log(removeDuplicate(string))
+//removing duplicates in array
+const arrr = [-1, 2, 0, 2, 7, 7, 7, -1, 0, 8, 3, 4];
+const nonDuplicatesarr = arrr.filter(el => arrr.indexOf(el) === arrr.lastIndexOf(el));
+console.log(nonDuplicatesarr)
+/////////
+let str = 'hello world'
+const arr = str.split('')
+const nonDuplicatesStr = arr.filter(el => arr.indexOf(el) === arr.lastIndexOf(el));
+console.log(nonDuplicatesStr.join(''))
